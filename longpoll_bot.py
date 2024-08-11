b72a2a1aeac055b8bd4acefbe2cfa4c5183c932b
samoha1337 <127851030+samoha1337@users.noreply.github.com>
2024-08-11 23:31:14 +0300

    Add files via upload

---
+++ longpoll_bot.py
@@ -0,0 +1,38 @@
+from simple_bot import Bot  # базовый класс бота из файла simple_bot
+
+from vk_api.longpoll import VkLongPoll, VkEventType  # использование VkLongPoll и VkEventType
+
+
+class LongPollBot(Bot):
+    """
+    Бот, прослушивающий в бесконечном цикле входящие сообщения и способный отвечать на некоторые из них
+    Бот отвечает на строго заданные сообщения
+    """
+
+    # длительное подключение
+    long_poll = None
+
+    def __init__(self):
+        """
+        Иинициализация бота
+        """
+        super().__init__()
+        self.long_poll = VkLongPoll(self.vk_session)
+
+    def run_long_poll(self):
+        """
+        Запуск бота
+        """
+        for event in self.long_poll.listen():
+
+            # если пришло новое сообщение - происходит проверка текста сообщения
+            if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+
+                # если была получена одна из заданных фраз
+                if event.text == "Привет" or event.text == "Здравствуй" or event.text == "Здравствуйте":
+
+                    # ответ отправляется в личные сообщения пользователя (если сообщение из личного чата)
+                    if event.from_user:
+                        self.send_message(receiver_user_id=event.user_id, message_text="Здравствуйте, Вас приветствует цифровой помощник главы Мирнинского района. Задайте Ваш вопрос.")
+
+
---
+++ main.py
@@ -0,0 +1,7 @@
+from simple_bot import Bot
+from longpoll_bot import LongPollBot
+from nlu_longpoll_bot import NLULongPollBot
+
+if __name__ == '__main__':
+    nlu_longpoll_bot = NLULongPollBot()
+    nlu_longpoll_bot.run_long_poll()
